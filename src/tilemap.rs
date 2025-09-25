1fa883b09e4441efc757407ad3d3b9662e60cdef
Kiyotoko <karl.zschiebsch@gmail.com>
2025-09-25 19:56:10 +0200

    feat: split tilemap in map and tiles

--- src/tilemap.rs
+++ src/tilemap.rs
@@ -5,9 +5,25 @@ pub trait TilemapLoader {
     fn parse(&self, data: &[u8]) -> Tilemap;
 }
 
+#[derive(Debug, Default)]
+pub struct Tile {
+    sprite: Vec2,
+    is_solid: bool,
+}
+
+impl Tile {
+    pub fn new(sprite: Vec2, is_solid: bool) -> Tile {
+        Tile { sprite, is_solid }
+    }
+}
+
 #[derive(Debug)]
 pub struct Tilemap {
-    rows: Vec<Vec<Vec2>>,
+    /// Map is the matrix of all tile id's.
+    /// A tile id is the index of the `tiles` vec.
+    map: Vec<Vec<Option<usize>>>,
+    /// Tiles is the vec that contains both the sprites and flags.
+    tiles: Vec<Tile>,
 }
 
 impl Tilemap {
@@ -18,9 +34,9 @@ impl Tilemap {
     pub fn draw_area(&self, spritesheet: &Spritesheet, rect: Rect, pos: Vec2, scale: f32) {
         for x in (rect.x as usize)..(rect.w as usize) {
             for y in (rect.y as usize)..(rect.h as usize) {
-                if let Some(sprite) = self.get_sprite(x, y) {
+                if let Some(tile) = self.get_tile(x, y) {
                     spritesheet.draw_sprite(
-                        sprite,
+                        &tile.sprite,
                         pos + vec2(
                             (x as f32) * spritesheet.sprite_width() * scale,
                             (y as f32) * spritesheet.sprite_height() * scale,
@@ -32,66 +48,81 @@ impl Tilemap {
         }
     }
 
-    pub fn get_sprite(&self, x: usize, y: usize) -> Option<&Vec2> {
-        if let Some(row) = self.rows.get(y) {
-            if let Some(v) = row.get(x) {
-                return if v.x == 0.0 && v.y == 0.0 {
-                    None
-                } else {
-                    Some(&v)
-                };
+    pub fn get_tile(&self, x: usize, y: usize) -> Option<&Tile> {
+        if let Some(row) = self.map.get(y) {
+            if let Some(tile) = row.get(x) {
+                if let Some(id) = tile {
+                    return self.tiles.get(*id);
+                }
             }
         }
+        // Outside of map
         None
     }
 }
 
 pub mod pico8 {
-    use crate::tilemap::{Tilemap, TilemapLoader};
+    use crate::tilemap::{Tile, Tilemap, TilemapLoader};
     use macroquad::prelude::*;
+    use std::collections::HashMap;
 
     #[derive(Debug, Default)]
     pub struct Loader {}
 
-    fn byte_to_float(byte: u8) -> f32 {
+    fn byte_to_hex(byte: u8) -> i8 {
         assert!(byte.is_ascii_hexdigit());
         if byte >= b'0' && byte <= b'9' {
-            return (byte - b'0') as f32;
+            return (byte - b'0') as i8;
         }
         if byte >= b'a' && byte <= b'f' {
-            return (byte - b'a' + 10) as f32;
+            return (byte - b'a' + 10) as i8;
         }
         if byte >= b'A' && byte <= b'F' {
-            return (byte - b'A' + 10) as f32;
+            return (byte - b'A' + 10) as i8;
         }
         panic!("could not convert byte to number");
     }
 
     impl TilemapLoader for Loader {
         fn parse(&self, data: &[u8]) -> Tilemap {
-            let mut cols = Vec::new();
-            let mut rows = Vec::new();
+            let mut pos2id = HashMap::new();
+            let mut map = Vec::new();
+            let mut tiles = Vec::new();
+            let mut col = Vec::new();
             let mut iter = data.iter();
 
             loop {
                 if let Some(next) = iter.next() {
                     if *next == b'\n' {
-                        rows.push(cols);
-                        cols = Vec::new();
+                        map.push(col);
+                        col = Vec::new();
                     } else {
-                        cols.push(vec2(
-                            byte_to_float(*iter.next().expect("expected pair")),
-                            byte_to_float(*next),
-                        ));
+                        let (x, y) = (
+                            byte_to_hex(*iter.next().expect("expected pair")),
+                            byte_to_hex(*next),
+                        );
+                        if x == 0 && y == 0 {
+                            col.push(None)
+                        } else {
+                            let tile_id = {
+                                if let Some(id) = pos2id.get(&(x, y)) {
+                                    *id
+                                } else {
+                                    tiles.push(Tile::new(Vec2::new(x as f32, y as f32), false));
+                                    let tile_id = tiles.len() - 1;
+                                    pos2id.insert((x, y), tile_id);
+                                    tile_id
+                                }
+                            };
+                            col.push(Some(tile_id));
+                        }
                     }
                 } else {
                     break;
                 }
             }
 
-            dbg!(rows.get(0));
-
-            Tilemap { rows }
+            Tilemap { map, tiles }
         }
     }
 }
